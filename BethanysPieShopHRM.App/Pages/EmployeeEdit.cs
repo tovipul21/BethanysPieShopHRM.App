3b7aeea53f07856abfae653f615388b0d40e14cc
Vipul Mehta <vipul.mehta@accenture.com>
2021-10-08 15:48:32 +0530

    Implemented Add, Update features to Employee Edit UI.

--- BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -18,6 +18,9 @@ namespace BethanysPieShopHRM.App.Pages
         [Inject]
         public IJobCategoryDataService JobCategoryService { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [Parameter]
         public string EmployeeId { get; set; }
 
@@ -30,8 +33,15 @@ namespace BethanysPieShopHRM.App.Pages
         public string CountryId { get; set; } = string.Empty;
         public string JobCategoryId { get; set; } = string.Empty;
 
+        // Store state of the screen
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
         protected async override Task OnInitializedAsync()
         {
+            Saved = false;
+
             int.TryParse(EmployeeId, out var employeeId);
 
             if (employeeId == 0) // New employee being created
@@ -44,24 +54,54 @@ namespace BethanysPieShopHRM.App.Pages
 
             Countries = (await CoutryDataService.GetAllCountries()).ToList();
             JobCategories = (await JobCategoryService.GetAllJobCategories()).ToList();
-
         }
 
         protected async Task HandleValidSubmit()
         {
+            Saved = false;
             Employee.JobCategoryId = int.Parse(CountryId);
             Employee.CountryId = int.Parse(CountryId);
 
             if (Employee.EmployeeId == 0)
             {
                 var addedEmployee = EmployeeService.AddEmployee(Employee);
+
+                if (addedEmployee != null)
+                {
+                    StatusClass = "alert-success";
+                    Message = "New employee added successfully!!";
+                    Saved = true;
+                }
+                else
+                {
+                    StatusClass = "alert-danger";
+                    Message = "Something went wrong adding new employeed. Plz try later";
+                    Saved = false;
+                }
             }
             else
-            {
                 await EmployeeService.UpdateEmployee(Employee);
-            }
+        }
 
+        protected void HandleInvalidSubmit()
+        {
+            StatusClass = "alert-danger";
+            Message = "Something went wrong adding new employeed. Plz try later";
+        }
 
+        protected async Task DeleteEmployee()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+
+            StatusClass = "alert-success";
+            Message = "Deleted successfully";
+
+            Saved = true;
+        }
+
+        protected void NavigateToOverview()
+        {
+            NavigationManager.NavigateTo("/employeeoverview");
         }
     }
 }
--- BethanysPieShopHRM.App/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.App/Services/EmployeeDataService.cs
@@ -17,14 +17,21 @@ namespace BethanysPieShopHRM.App.Services
             _httpClient = httpClient;
         }
 
-        public Task<Employee> AddEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+
+            if (response.IsSuccessStatusCode)
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+
+            return null;
         }
 
-        public Task DeleteEmployee(int employeeId)
+        public async Task DeleteEmployee(int employeeId)
         {
-            throw new NotImplementedException();
+            await _httpClient.DeleteAsync($"api/employee{ employeeId }");
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployees()
@@ -39,9 +46,11 @@ namespace BethanysPieShopHRM.App.Services
                 (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), System.Text.Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("api/employee", employeeJson);
         }
     }
 }
