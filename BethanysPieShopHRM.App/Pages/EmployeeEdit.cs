425a3947867f50e167441c36b2e1e0fae531b4c0
Vipul Mehta <vipul.mehta@accenture.com>
2021-10-08 14:14:05 +0530

    * Updated Employee edit form by adding new components. * Created JobCategoryDataService class and interface. * Applied dependency injection for JobCategoryDataService

--- BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -15,6 +15,9 @@ namespace BethanysPieShopHRM.App.Pages
         [Inject]
         public ICountryDataService CoutryDataService { get; set; }
 
+        [Inject]
+        public IJobCategoryDataService JobCategoryService { get; set; }
+
         [Parameter]
         public string EmployeeId { get; set; }
 
@@ -22,15 +25,43 @@ namespace BethanysPieShopHRM.App.Pages
 
         public List<Country> Countries { get; set; } = new List<Country>();
 
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+
         public string CountryId { get; set; } = string.Empty;
+        public string JobCategoryId { get; set; } = string.Empty;
 
         protected async override Task OnInitializedAsync()
         {
-            Employee = await EmployeeService.GetEmployeeDetails(int.Parse(EmployeeId));
-            
+            int.TryParse(EmployeeId, out var employeeId);
+
+            if (employeeId == 0) // New employee being created
+                Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = System.DateTime.Now, JoinedDate = System.DateTime.Now };
+            else // Existing employee being updated
+                Employee = await EmployeeService.GetEmployeeDetails(int.Parse(EmployeeId));
+
             CountryId = Employee.CountryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
 
             Countries = (await CoutryDataService.GetAllCountries()).ToList();
+            JobCategories = (await JobCategoryService.GetAllJobCategories()).ToList();
+
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            Employee.JobCategoryId = int.Parse(CountryId);
+            Employee.CountryId = int.Parse(CountryId);
+
+            if (Employee.EmployeeId == 0)
+            {
+                var addedEmployee = EmployeeService.AddEmployee(Employee);
+            }
+            else
+            {
+                await EmployeeService.UpdateEmployee(Employee);
+            }
+
+
         }
     }
 }
---
+++ BethanysPieShopHRM.App/Services/IJobCategoryDataService.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShopHRM.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Services
+{
+    public interface IJobCategoryDataService
+    {
+        Task<IEnumerable<JobCategory>> GetAllJobCategories();
+
+        Task<JobCategory> GetJobCategoryById(int countryid);
+    }
+}
