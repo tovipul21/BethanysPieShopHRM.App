4253249b256660aeae5e21a2cd6c81a6a1085c92
Vipul Mehta <tovipul21@gmail.com>
2021-10-09 13:04:04 +0530

    * Created new component files for Employee dialog file. * Modified existing files to cater for new employee dialog component.

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -1,7 +1,7 @@
-﻿using BethanysPieShopHRM.App.Services;
+﻿using BethanysPieShopHRM.App.Components;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,9 +15,22 @@ namespace BethanysPieShopHRM.App.Pages
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
         }
-	}
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
+        }
+    }
 }
